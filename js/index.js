6b88172e44531c6de508a1b063b5f471cc3d8982
xiaoswaii <b05106028@ntu.edu.tw>
2019-05-27 22:33:08 +0800

    finish

--- js/index.js
+++ js/index.js
@@ -1,37 +1,83 @@
 $(document).ready(function(){
+    var newPage=(totalItem)=>{
+        var totalPage = Math.round(totalItem/20);
+        for(var i =1;i<=totalPage;i++){
+            var li = `<li id="page${i}" class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+            $('#page-number').append(li);
+        }
+    }
+
+    $('body').on('click','.page-item',function(){
+        $('#priceUpper').val('');
+        $('#priceLower').val('');
+        $('#product-list').empty()
+        $('.page-item').attr('class','page-item');
+        console.log($(this).text());
+        $(this).attr('class','page-item active');
+        var toDisplayRange = parseInt($(this).text());
+        toDisplayRange = (toDisplayRange * 20) ;
+        var startDisplay = toDisplayRange - 20;
+        $.ajax({
+            url:'https://js.kchen.club/B05106052/query',
+            type:'GET',
+            success:function(data){
+                var array=data.items
+                for(var i = startDisplay;i<toDisplayRange;i++){
+                    var li = `<div class="col-*"><div class="item"><img src="${array[i].image}" class="image"><h3 class="name">${array[i].name}</h3><p class="price">NTD ${array[i].price}</p></div></div>`;
+                    $('#product-list').append(li);
+                }
+            },
+            error:function(e){
+                alert(e);
+            }
+        })
+    })
+
     $.ajax({
         url:'https://js.kchen.club/B05106052/query',
         type:'GET',
         success:function(data){
-            console.log(data);
-            var array=data.items
-            array.forEach(data => {
-                var li = `<div class="col-*"><div class="item"><img src="${data.image}" class="image"><h3 class="name">${data.name}</h3><p class="price">NTD ${data.price}</p></div></div>`
-                $('#product-list').append(li)
-            });
+            var array=data.items;
+            newPage(array.length);
+            $('#page1').attr('class','page-item active');
+            for(var i = 0;i<20;i++){
+                var li = `<div class="col-*"><div class="item"><img src="${array[i].image}" class="image"><h3 class="name">${array[i].name}</h3><p class="price">NTD ${array[i].price}</p></div></div>`;
+                $('#product-list').append(li);
+            }
+        },
+        error:function(e){
+            alert(e);
         }
     })
 
-    $('#query').click(function(){
-        $('#product-list').empty();
+    $('body').on('click','#query',function(){
+        $('#priceUpper').val('');
+        $('#priceLower').val('');
+        $('#product-list').empty(); 
         $.ajax({
             url:'https://js.kchen.club/B05106052/query',
             type:'GET',
             success:function(data){
                 console.log(data);
                 var array=data.items
-                array.forEach(data => {
-                    var li = `<div class="col-*"><div class="item"><img src="${data.image}" class="image"><h3 class="name">${data.name}</h3><p class="price">NTD ${data.price}</p></div></div>`
-                    $('#product-list').append(li)
-                });
+                $('.page-item').attr('class','page-item');
+                $('#page1').attr('class','page-item active');
+                for(var i = 0;i<20;i++){
+                    var li = `<div class="col-*"><div class="item"><img src="${array[i].image}" class="image"><h3 class="name">${array[i].name}</h3><p class="price">NTD ${array[i].price}</p></div></div>`;
+                    $('#product-list').append(li);
+                }
+            },
+            error:function(e){
+                alert(e);
             }
         })
     });
+
+
+
     var checkPriceFilter=()=>{
         var priceUpper = parseInt($('#priceUpper').val());
         var priceLower = parseInt($('#priceLower').val());
-        console.log(priceLower)
-        console.log(priceUpper)
         if(!priceLower || !priceUpper){
             return '1';
         }
@@ -47,7 +93,6 @@ $(document).ready(function(){
     }
 
     $('#priceFilter').click(function(){
-        console.log(checkPriceFilter());
         if(checkPriceFilter() === '1'){
             alert("Please Fill Both Upper And Lower!")
         }
@@ -65,6 +110,7 @@ $(document).ready(function(){
                 url:'https://js.kchen.club/B05106052/query',
                 type:'GET',
                 success:function(data){
+                    $('.page-item').attr('class','page-item');
                     var array=data.items
                     var li = array
                                 .filter(item => item.price<priceUpper && item.price>priceLower)
@@ -77,3 +123,4 @@ $(document).ready(function(){
          
     })
 })
+
