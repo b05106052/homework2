2c042dd25e6a48facc46060ae4c5fd159f2d41a0
xiaoswaii <b05106028@ntu.edu.tw>
2019-05-29 14:05:18 +0800

    addNewItem fix

--- js/addNewItem.js
+++ js/addNewItem.js
@@ -1,26 +1,30 @@
-$('#addNewItem').click(function(){
+$('#addNewItem').on('click',function(){
     var itemName = $('#itemName').val();
     var itemPrice = parseInt($('#itemPrice').val());
     var itemAmount = parseInt($('#itemAmount').val());
     var itemPicUrl = $('#itemPicUrl').val();
-    var dataToPost = JSON.stringify({
+    var dataToPost = {
         item:{
             name:itemName,
             price:itemPrice,
             count:itemAmount,
             image:itemPicUrl
-        }});
-    console.log(dataToPost)
-    $.ajax({
-        type:'POST',
-        dataType: 'json',
-        data:dataToPost,
-        url:'https://js.kchen.club/B05106052/insert',
-        success:function(returnMsg){
-            console.log(returnMsg)
-        },
-        error:function(errorMsg){
-            console.log(errorMsg)
-        }
-    })
+        }};
+    $.post('https://js.kchen.club/B05106052/insert', dataToPost, function(response) {
+            if (response) {
+                // 伺服器有回傳資料
+                if (response.result) {
+                    $('#message').text('新增成功')
+                    console.log(response.item)
+                    $('#dialog').modal('show')
+                } else {
+                    $('#message').text('新增失敗')
+                    console.log(response.message)
+                    $('#dialog').modal('show')
+                }
+            } else {
+                $('#message').text('伺服器出錯')
+                $('#dialog').modal('show')
+            }
+        })
 });
