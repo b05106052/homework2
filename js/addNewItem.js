4bd737a89391d0e5fe3c48dfcb043ff4d0cd25e0
xiaoswaii <b05106028@ntu.edu.tw>
2019-05-29 14:08:25 +0800

    addNewItem ajax FIX

--- js/addNewItem.js
+++ js/addNewItem.js
@@ -1,4 +1,4 @@
-$('#addNewItem').on('click',function(){
+$('#addNewItem').click(function(){
     var itemName = $('#itemName').val();
     var itemPrice = parseInt($('#itemPrice').val());
     var itemAmount = parseInt($('#itemAmount').val());
@@ -10,21 +10,18 @@ $('#addNewItem').on('click',function(){
             count:itemAmount,
             image:itemPicUrl
         }};
-    $.post('https://js.kchen.club/B05106052/insert', dataToPost, function(response) {
-            if (response) {
-                // 伺服器有回傳資料
-                if (response.result) {
-                    $('#message').text('新增成功')
-                    console.log(response.item)
-                    $('#dialog').modal('show')
-                } else {
-                    $('#message').text('新增失敗')
-                    console.log(response.message)
-                    $('#dialog').modal('show')
-                }
-            } else {
-                $('#message').text('伺服器出錯')
-                $('#dialog').modal('show')
-            }
-        })
+
+    $.ajax({
+        type:'POST',
+        data:dataToPost,
+        url:'https://js.kchen.club/B05106052/insert',
+        success:function(returnMsg){
+            $('#message').text('新增成功')
+            console.log(returnMsg)
+        },
+        error:function(errorMsg){
+            $('#message').text('新增失敗')
+            console.log(errorMsg)
+        }
+    })
 });
